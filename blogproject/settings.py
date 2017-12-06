cc350905c173f514c355c95f9e27bd7e4a3b5d12
shancun <sc@lairen.com>
2017-12-06 12:08:35 +0800

    修改ui

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -114,7 +114,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-
+# 加入下面的配置
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # django-haystack
 HAYSTACK_CONNECTIONS = {
     'default': {
